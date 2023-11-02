e30854869b4be279d0ba51b478535299fe8bc270
Smoothy1 <saltydata@gmail.com>
2023-11-02 23:20:40 +0400

    debug

--- avito_scam/urls.py
+++ avito_scam/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
+    urlpatterns = [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]+urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
